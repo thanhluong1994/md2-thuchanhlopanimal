f332de971e511dcf0108736e505ba06b121d22da
unknown <luongvu2605@gmail.com>
2022-02-15 13:29:21 +0700

    lopAnimal

---
+++ src/Chicken.java
@@ -0,0 +1,12 @@
+public class Chicken extends Animal implements Edible{
+
+    @Override
+    public String makeSound(){
+        return "Chicken:cluck-cluck!";
+    }
+
+    @Override
+    public String howtoEat() {
+        return "dang an";
+    }
+}
